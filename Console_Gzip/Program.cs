a480b431b95e2a52e175b654072258ce14f179ba
doktor alban <doktoralban@outlook.com>
2020-07-06 21:27:48 +0300

    Initial commit

---
+++ Console_Gzip/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Console_Gzip
+{
+    class Program
+    {
+        static string fileName = @"C:\testC\111.txt";
+        static string outputFileName = @"C:\testC\111.gzip"; 
+        static void Main(string[] args)
+        {
+            Compress();
+            deCompress();
+        }
+        static void Compress()
+        { 
+            try
+            {
+                using (FileStream inputStream = new FileStream(fileName, FileMode.OpenOrCreate,FileAccess.ReadWrite))
+                {
+                    using (FileStream outputStream = new FileStream(outputFileName,FileMode.OpenOrCreate,FileAccess.ReadWrite))
+                    {
+                        using (GZipStream gzip = new GZipStream(outputStream,CompressionMode.Compress))
+                        {
+                            inputStream.CopyTo(gzip);
+                        } 
+                    } 
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+        //..........................................................
+        static void deCompress()
+        { 
+            try
+            {
+                using (FileStream inputStream = new FileStream(outputFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (FileStream outputStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+                        using (GZipStream gzip = new GZipStream(inputStream, CompressionMode.Decompress))
+                        {
+                            gzip.CopyTo(outputStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+        //..........................................................
+
+
+    }
+}
